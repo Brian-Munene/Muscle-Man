9345bcd803a9dae3088f40ae5a5a3906a895b1f3
Brian Munene <brianmunene69@gmail.com>
2018-07-04 10:35:56 +0300

    Routes

--- routes/api.php
+++ routes/api.php
@@ -25,24 +25,12 @@ Route::get('/instructors', 'instructor94233Controller@index');
 //View a single instructor
 Route::get('/instructor/{id}', 'instructor94233Controller@show');
 //Add a new workout session
-Route::post('/session',[ 
-'middleware' => 'auth:api',
-'uses' => 'session94233Controller@store'
-]);
+Route::post('/session','session94233Controller@store');
 //View workout sessions
-Route::get('/sessions/{id}',[ 
-    'middleware' => 'auth:api',
-    'uses' => 'session94233Controller@show'
-    ]);
+Route::get('/sessions/{id}','session94233Controller@show');
 //view users profile
-Route::get('/users', [ 
-    'middleware' => 'auth:api',
-    'uses' => 'user94233Controller@index'
-    ]);
+Route::get('/users','user94233Controller@index');
 //view user profile
-Route::get('/user/{id}', [ 
-    'middleware' => 'auth:api',
-    'uses' => 'user94233Controller@show'
-    ]);
+Route::get('/user/{id}','user94233Controller@show');
 //List all gym locations
 Route::get('/locations', 'gym_location94233Controller@index');
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//login and sign up 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home'); 
 //List all instructors
@@ -21,24 +22,12 @@ Route::get('/instructors', 'instructor94233Controller@index');
 //View a single instructor
 Route::get('/instructor/{id}', 'instructor94233Controller@show');
 //Add a new workout session
-Route::post('/session',[ 
-'middleware' => 'auth',
-'uses' => 'session94233Controller@store'
-]);
+Route::post('/session','session94233Controller@store');
 //View workout sessions
-Route::get('/sessions',[ 
-    'middleware' => 'auth',
-    'uses' => 'session94233Controller@show'
-    ]);
+Route::get('/sessions/{id}','session94233Controller@show');
 //view users profile
-Route::get('/users', [ 
-    'middleware' => 'auth',
-    'uses' => 'user94233Controller@index'
-    ]);
+Route::get('/users','user94233Controller@index');
 //view user profile
-Route::get('/user/{id}', [ 
-    'middleware' => 'auth',
-    'uses' => 'user94233Controller@show'
-    ]);
+Route::get('/user/{id}','user94233Controller@show');
 //List all gym locations
 Route::get('/locations', 'gym_location94233Controller@index');
